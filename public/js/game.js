a8d83bce0b3a7ca085fd143ead2b74d35672b021
Michael Acker <doby162@gmail.com>
2016-07-16 07:36:39 -0500

    asteroid collisions

--- public/js/game.js
+++ public/js/game.js
@@ -14,12 +14,17 @@ var hero;
 var asteroids;
 var map; // For Tilemap
 var layer; // For Tilemap
+var asteroidCollisionGroup; //these are basically collision layers with defined overlaps
+var heroCollisionGroup;
 
 function create() {
     asteroids = game.add.group();
-
     game.physics.startSystem(Phaser.Physics.P2JS);
-
+    game.physics.p2.restitution = 0.8; //no idea what this means
+    asteroidCollisionGroup = game.physics.p2.createCollisionGroup();
+    heroCollisionGroup = game.physics.p2.createCollisionGroup();
+    game.physics.p2.setImpactEvents(true); //yes I do want callbacks
+    game.physics.p2.updateBoundsCollisionGroup(); //yes i do want the edge of the screen to be a wall 
     game.stage.backgroundColor = '#0072bc';
 
     // For Tilemap
@@ -34,13 +39,18 @@ function create() {
 
     cursor = game.input.keyboard.createCursorKeys();
     hero = game.add.sprite(256, game.world.height - 150, 'hero');
+    hero.health = 5;
+console.log(hero.health);
     hero.scale = new Phaser.Point(2, 2);
     game.physics.p2.enable(hero);
 
     for (var i = 0; i < 10; i++) {
         var asteroid = asteroids.create(game.rnd.integerInRange(200, 1700), game.rnd.integerInRange(-200, 400), 'asteroid');
         game.physics.p2.enable(asteroid, false);
+        asteroid.body.setCollisionGroup(asteroidCollisionGroup);
+        asteroid.body.collides([asteroidCollisionGroup, heroCollisionGroup]);
     }
+    hero.body.collides(asteroidCollisionGroup, hitsteroid, this);
 
     //  Enable Arcade Physics for the sprite
     game.physics.enable(mousePointerSprite, Phaser.Physics.ARCADE);
@@ -51,12 +61,24 @@ function create() {
     // For bouncing off of Tilemap
     mousePointerSprite.body.bounce.set(0.6);
     mousePointerSprite.body.tilePadding.set(32);
+    sprite.body.allowRotation = false;
+    hero.body.setCollisionGroup(asteroidCollisionGroup);
 
+}
+function hitsteroid(body1, body2) {
+
+    //  body1 is the space ship (as it's the body that owns the callback)
+    //  body2 is the body it impacted with, in this case our panda
+    //  As body2 is a Phaser.Physics.P2.Body object, you access its own (the sprite) via the sprite property:
+//    body2.sprite.alpha -= 0.1; example code
+hero.health--;
 }
 
 function update() {
 
     mousePointerSprite.rotation = game.physics.arcade.moveToPointer(mousePointerSprite, 60, game.input.activePointer, 500);
+    
+    sprite.rotation = game.physics.arcade.moveToPointer(sprite, 60, game.input.activePointer, 500);
     if (cursor.left.isDown) {hero.body.rotateLeft(100);}
     else if (cursor.right.isDown) {hero.body.rotateRight(100);}
     else {hero.body.setZeroRotation();}

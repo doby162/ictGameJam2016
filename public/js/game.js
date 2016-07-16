c7b809469d1d57a64ac67cc0a36934d6ac70f28d
Michael Acker <doby162@gmail.com>
2016-07-15 20:34:05 -0500

    make a little jetpack person

--- public/js/game.js
+++ public/js/game.js
@@ -6,15 +6,21 @@ function preload() {
 }
 
 var sprite;
+var cursor;
+var hero;
 
 function create() {
 
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.startSystem(Phaser.Physics.P2JS);
 
     game.stage.backgroundColor = '#0072bc';
 
     sprite = game.add.sprite(400, 300, 'arrow');
     sprite.anchor.setTo(0.5, 0.5);
+
+    cursor = game.input.keyboard.createCursorKeys();
+    hero = game.add.sprite(32, game.world.height - 150, 'arrow');
+    game.physics.p2.enable(hero);
 
     //  Enable Arcade Physics for the sprite
     game.physics.enable(sprite, Phaser.Physics.ARCADE);
@@ -27,6 +33,11 @@ function create() {
 function update() {
 
     sprite.rotation = game.physics.arcade.moveToPointer(sprite, 60, game.input.activePointer, 500);
+    if (cursor.left.isDown) {hero.body.rotateLeft(100);}
+    else if (cursor.right.isDown) {hero.body.rotateRight(100);}
+    else {hero.body.setZeroRotation();}
+    if (cursor.up.isDown) {hero.body.thrust(400);}
+    if (cursor.down.isDown) {hero.body.thrust(400);}
 
 }
 

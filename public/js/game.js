ceb3d76a3d760b35ee1c0b405cebb182a008d59a
Michael Acker <doby162@gmail.com>
2016-07-15 20:59:34 -0500

    scale

--- public/js/game.js
+++ public/js/game.js
@@ -2,7 +2,7 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
-    game.load.image('arrow', 'assets/hero1.png');
+    game.load.image('hero', 'assets/hero1.png');
 }
 
 var sprite;
@@ -15,11 +15,12 @@ function create() {
 
     game.stage.backgroundColor = '#0072bc';
 
-    sprite = game.add.sprite(400, 300, 'arrow');
+    sprite = game.add.sprite(400, 300, 'hero');
     sprite.anchor.setTo(0.5, 0.5);
 
     cursor = game.input.keyboard.createCursorKeys();
-    hero = game.add.sprite(32, game.world.height - 150, 'arrow');
+    hero = game.add.sprite(256, game.world.height - 150, 'hero');
+    hero.scale = new Phaser.Point(2, 2);
     game.physics.p2.enable(hero);
 
     //  Enable Arcade Physics for the sprite
